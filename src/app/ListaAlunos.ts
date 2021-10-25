7e01a058b47fda980acb3431ec0597f86c49af31
SulivanGalahad <sulivanpm@gmail.com>
2021-10-25 13:08:40 -0300

    Ap3

---
+++ src/app/ListaAlunos.ts
@@ -0,0 +1,7 @@
+export const Alunos = [
+    { nome: 'Sulivan Maciel de Oliveira', ru: 2574483, curso: 'Análise e Desenvolvimento de Sistemas', dn: new Date(1972, 9, 25) },
+    { nome: 'Capizomba Gumercindovandro Junior', ru: 1233210, curso: 'Filosofia Quântica Política', dn: new Date(1970, 4, 23) },
+    { nome: 'Javacreisson Felisbilson Cadaquantus', ru: 9588591, curso: 'Ciência Científica Doméstica', dn: new Date(1964, 3, 13) },
+    { nome: 'Cesharpiano Polistriquino Jupir', ru: 6050409, curso: 'Filantropia Libertária Quadrilátera', dn: new Date(1980, 7, 29) },
+    { nome: 'Godoyana Pirilampina Universitildes', ru: 3096476, curso: 'Magia Lógica Aplicada', dn: new Date(1902, 2, 22) },
+]
